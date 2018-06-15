00e227be7ac08d60653fba548f5af56ed1e51864
tianqibucuohao <lichzhenglz@gmail.com>
2018-06-16 00:17:19 +0800

    order list

--- Server/DB/db.php
+++ Server/DB/db.php
@@ -37,7 +37,7 @@ public function Query($sql){
 //	else 
 //		echo "conn is alive<br />";
 	$result = mysql_query($sql,$this->dbh);
-//	echo $sql;
+#	echo $sql;
 	if ($result){
 		$i=0;
 		while ($rol=mysql_fetch_array($result))
@@ -48,9 +48,9 @@ public function Query($sql){
 
 //		echo "<br />not null, array.size=". count($arry);
 	}
-	else { 
-		echo "null";
-	}
+#	else { 
+#		echo "null";
+#	}
 //	echo "<br />########<br />";
 	return $arry;
 }
@@ -109,8 +109,93 @@ public function GetAllGoods() {
 	$sql = "select a.g_id, a.g_name, a.g_price, a.g_sortid, a.g_discountid, a.g_desc, a.g_pricetype,a.g_bAd, b.i_name from goodstbl a, imgpathtbl b where a.g_id = b.gs_id group by b.gs_id";
 	if (!$this->dbh)
 		return $arry;
-	$ret = $this->Query($sql);
-	return $ret;
+#	$ret = $this->Query($sql);
+#	return $ret;
+}
+public function NewBills($ukey, $count, $sum, $ddate){
+	$sts = 1;
+	$note = "";
+	$arr = array();
+	if ($ukey == "") {
+		return $arr;
+	}
+	
+	$ins = "insert into billlisttbl(b_uuid, b_count, b_total, b_status, b_datetime, b_note, b_dnow) value('%s',%d, %.2f, %d, '%s', '%s', now());";
+	$sql = sprintf($ins, $ukey, $count, $sum, $sts, $ddate, $note);
+#	echo 'ins==>'.$sql;
+#	$b["sql"] = $sql;
+#	echo json_encode($b);
+	if (!$this->dbh){
+		return ['err'=>"error"];
+	}
+	$this->Query($sql);
+	$sql = "select b_id from billlisttbl where b_uuid='".$ukey."'";
+#	echo $sql;
+	$res = $this->Query($sql);
+	return $res;
+}
+public function BillsDetail($arrSql) {
+	$ary = array();	
+	if ($arrSql == ""){
+		return $ary;
+	}
+	if (!$this->dbh) {
+		return $ary;
+	}
+	$this->Query($arrSql);
+}
+public function CnlBills($id, $ukey){
+	if ($id == -1 || $id == 0) {
+		return MsgToClient(0, 'idx errot');
+	}
+	$upd = "update billlisttbl set b_status=%d where b_uuid='%s'";
+	$sql = sprintf($upd, $id, $ukey);
+	if (!this->dbh){
+		return MsgToClient(0, 'data error');
+	}
+	$this->Query($sql);
+}
+
+public function GetHistoryAllBills($ukey, $strDate, $endDate) {
+	# 取回所有成功订单id ，某时段内
+	# 成功取消的订单不查询
+	if ($ukey == "") {
+		return MsgToClient(0, 'can`t find history');
+	}
+	$ins = "";
+	$sql = "";
+	if ($strDate == "" || $endDate == "") {
+		#所有
+		$ins = "select b_id, b_count, b_total, b_note from billlisttbl where b_status = 1 and b_uuid='%s'";
+		$sql = sprintf($ins, $ukey);
+	}else {
+		#时段内 - 不会写
+		$ins = "";
+	}
+	if ($this->dbh) {
+		return MsgToClient(0, 'data error');
+	}
+	$res = $this->Query($sql);
+	return $res;
+}
+public function GetHistoryAllBillsDetail($bid) {
+	#取回详细订单内容
+	$ary = array();
+	if ($ukey == "") {
+		return MsgToClient(0, 'can`t find history');
+	}
+	
+	if ($this->dbh) {
+		return MsgToClient(0, 'data error');
+	}
+	$ins = "select g_id, g_name, g_count, g_price, g_pricetype, g_total from order_details where b_id = %d";
+	$sql = sprintf($ins, $bid);
+	$res = $this->Query($sql);
+	return $res;
+}
+
+public function MsgToClient($id, $msg){
+	return json_decode(['err'=>$id, 'msg'=>$msg]);
 }
 /*
  function storeUserInfoToDB($userinfo, $skey, $sessionKey) {
--- Server/gen.php
+++ Server/gen.php
@@ -1,4 +1,60 @@
 <?php
+require_once('DB/db.php');
+require_once('class/class-goods.php');
+
 $ret = file_get_contents('php://input');
-echo $ret;
+#echo $ret;
+$rtn = [];
+try{
+$arr = json_decode($ret, true);
+$db = new DB();
+$ary = [
+"goods"=>[]
+];
+if ($arr){
+	if (count($arr)!=0){
+		$key = $arr['key'];
+		$bill = $arr['bill'];
+		$sum = $arr['sum'];
+		$dtime = $arr['date'];
+		$len = count($bill);
+#		echo 'key -->'+$key;
+		if ($key == '') {
+			$rtn = ['err'=>0,'msg'=>'key is emtpy'];
+		}
+		$ret = $db->NewBills($key, $len, $sum, $dtime);
+		$len = count($ret);
+		$bid = -1;
+		if ($len > 0){
+			$bid = $ret[0]['b_id'];
+		}
+		if ($bid == -1) {
+			$rtn = ['err'=>0,'msg'=>$id];
+		} else {
+			$ins = "insert into order_details(g_id, g_name, g_count, g_price, g_pricetype, g_total, b_id) value(%d, '%s', %d, %.2f, '%s', %.2f, %d);";
+			for ($i=0; $i<count($bill); $i++) {
+				$id = $bill[$i]['id'];
+				$name = $bill[$i]['name'];
+				$count = $bill[$i]['count'];
+				$price = $bill[$i]['price'];
+				$pricetype = $bill[$i]['pricetype'];
+				$total = $bill[$i]['sum'];
+				$v = sprintf($ins, $id, $name, $count, $price, $pricetype, $total, $bid);
+				
+				$db->BillsDetail($v);
+			}
+			$rtn = ['err'=>1,
+					'msg'=>"suc"];
+		}
+	}else {
+	 $rtn = ['err'=>0,
+		'msg'=>"no data"];
+	}
+}
+} catch(Exception $e){
+	$rtn = ['err'=>0,
+		'msg'=>$e->getMessage()
+		];
+}
+echo json_encode($rtn);
 ?>
